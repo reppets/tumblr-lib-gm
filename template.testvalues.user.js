78d3b09622539e0c16850df4a95ee2d7c4cd6c4a
reppets <all.you.need.is.word@gmail.com>
2017-05-14 01:04:02 +0900

    add some tweak on testscript

---
+++ template.testvalues.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        tumblr-lib-gm-testvalues
+// @namespace   reppets.net
+// @version     1.0.0
+// @grant       none
+// ==/UserScript==
+var Values = {
+	consumerKey:'',
+	consumerSecret: ''
+};
--- test.user.js
+++ test.user.js
@@ -3,15 +3,28 @@
 // @namespace   reppets.net
 // @version     1.0.0
 // @require     tumblr.user.js
+// @require     testvalues.user.js
 // @require     https://cdn.rawgit.com/ddo/oauth-1.0a/2.1.0/oauth-1.0a.js
-// @include     http://reppets.net/github-test/
+// @include     http://reppets.net/tumblr-lib-gm/test.html*
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
+
 try {
-	var consumerKey = '';
-	var consumerSecret = '';
+	console.log ('run');
+	var consumerKey = Values.consumerKey;
+	var consumerSecret = Values.consumerSecret;
 	var accessToken = '';
 	var accessTokenSecret = '';
+
+	console.log(typeof window.location.search);
+	if (window.location.search.indexOf('oauth_token')>0 && window.location.search.indexOf('oauth_verifier')>0 && window.opener) {
+		var params = splitParameter(window.location.search);
+		console.log(window.opener);
+		//window.opener.oauthCallback(params.oauth_token, params.oauth_verifier);
+		window.eval("window.opener.oauthCallback('"+params.oauth_token+"','"+params.oauth_verifier+"');");
+		return;
+	}
+
 
 	var root = document.querySelector('body');
 
@@ -34,11 +47,12 @@ try {
 	}
 
 	var client = new Tumblr(consumerKey, consumerSecret, Tumblr.LOG_DEBUG);
-	client.getRequestToken('http://reppets.net/', {onload: Tumblr._log('onload',function(res){
+	client.getRequestToken('http://reppets.net/tumblr-lib-gm/test.html', {onload: Tumblr._log('onload',function(res){
 		console.log(res);
 		console.log(client.getAuthorizeURL(splitParameter(res.responseText).oauth_token));
+		window.eval("window.oauthCallback = function(token,verifier) {console.log('callback', token, verifier);};");
 		window.open(client.getAuthorizeURL(splitParameter(res.responseText).oauth_token));
 	})});
 } catch (e) {
-	write('<div>Error: '+e+'</div>');
+	console.log(e);
 }
